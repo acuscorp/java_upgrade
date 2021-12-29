f6d4d22e88f0b8635b36484c3d5bc843a4d98039
Noe Acuna <57365344+acuscorp@users.noreply.github.com>
2021-12-29 14:26:07 -0600

    Add FileNameFilter test and lazy supplier

--- src/test/java/lambdas/FileFilterTest.java
+++ src/test/java/lambdas/FileFilterTest.java
@@ -76,25 +76,46 @@ public class FileFilterTest {
 
     @Test
     public void listDirectories_methodReference() {
+        File[] directories = root.listFiles(File::isDirectory);
 
+        if (directories != null) {
+            assertEquals(13,directories.length);
+        }
     }
 
     @Test
     public void javaSourceFiles() {
-
+        FilenameFilter filter = (dir, name) -> name.endsWith(".java");
+        File[] javaFiles = root.listFiles(filter);
+        if (javaFiles != null) {
+            assertEquals(8,javaFiles.length);
+        }
     }
 
     private String getErrorMessage() {
-        return "";
+        System.out.println("Inside getErrorMethod");
+        return Thread.currentThread() + "This is my error message";
     }
 
     @Test
     public void notNullArgument() {
+        String x = "abc";
+        assertNotNull(x, getErrorMessage());
 
+        String y = "abc";
+        assertNotNull(null, () -> getErrorMessage());
+
+        Supplier<String> supplier = () -> getErrorMessage();
+        String z = "ABC";
+        assertNotNull(z, supplier);
     }
 
     @Test
     void iterateOverListOrMap() {
-
+        List<String> strings = Arrays.asList("this", "is", "a", "list");
+        strings.forEach(System.out::println);
+        Map<String,Integer> map = new HashMap<>();
+        map.put("a",1); map.put("b",2); map.put("c",3);
+        map.forEach((k,v)-> System.out.println(k+" maps to " + v));
     }
 }
